04ede3bc5b2eabb1beb9ffa570d259d754fbc2cc
GitHub Action <action@github.com>
2020-11-01 21:28:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/felipetozato/intellijplugindataclassbuilder/MyBundle.kt
+++ src/main/kotlin/com/github/felipetozato/intellijplugindataclassbuilder/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.felipetozato.intellijplugindataclassbuilder
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/felipetozato/intellijplugindataclassbuilder/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.felipetozato.intellijplugindataclassbuilder.services
+
+import com.github.felipetozato.intellijplugindataclassbuilder.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
